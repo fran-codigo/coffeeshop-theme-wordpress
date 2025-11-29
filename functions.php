15f93308a8ae801b38d317c08d3da8ead3510073
Franco <franco_tigres18@hotmail.com>
2025-11-29 09:24:44 -0600

    feat:function to get featured images with a format

--- functions.php
+++ functions.php
@@ -7,3 +7,42 @@ function coffee_shop_setup()
 }
 
 add_action('after_setup_theme', 'coffee_shop_setup');
+
+/* Esta función sirve para agregar campos personalizados 
+a la API REST de WordPress */
+function coffee_shop_api_init()
+{
+    register_rest_field(
+        ['page'],
+        'feautured_images',
+        ['get_callback' => 'get_featured_image']
+    );
+}
+
+add_action('rest_api_init', 'coffee_shop_api_init');
+
+/* Esta funcion obtiene las imágenes destacadas en varios tamaños 
+y es la que se agrega a la API REST*/
+function get_featured_image($post)
+{
+    if (!$post['featured_media']) {
+        return false;
+    }
+
+    $image_sizes = get_intermediate_image_sizes();
+    $images = [];
+    foreach ($image_sizes as $size) {
+
+        if ($size === '2048x2048') {
+            continue;
+        }
+
+        $image = wp_get_attachment_image_src($post['featured_media'], $size);
+        $images[$size === '1536x1536' ? 'full' : $size] = [
+            'url' => $image[0],
+            'width' => $image[1],
+            'height' => $image[2],
+        ];
+    }
+    return $images;
+}
